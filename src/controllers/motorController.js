f26ea629e548aa2493663cde11d94971806fed4d
developer-wenxt <developer@wenxttech.com>
2025-10-15 16:44:00 +0530

    pushing template and instance policy_01

--- src/controllers/motorController.js
+++ src/controllers/motorController.js
@@ -1,8 +1,21 @@
 const { getMotor } = require('../services/motorService');
 
+// exports.getMotor = async (req, res) => {
+//   try {
+//     const data = await getMotor();
+//     res.json(data);
+//   } catch (error) {
+//     console.error('Controller error:', error);
+//     res.status(500).json({ error: 'Failed to fetch class_of_bus data' });
+//   }
+// };
+
+
+
 exports.getMotor = async (req, res) => {
   try {
-    const data = await getMotor();
+    const { prodCode, instCode, busType } = req.query;
+    const data = await getMotor(prodCode, instCode, busType);
     res.json(data);
   } catch (error) {
     console.error('Controller error:', error);
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const lmCompanyRoutes=require('./lmCompanyRoutes');
 const class_of_busRoute = require('./class_of_busRoute');
 const motorRoute =require('./motorRoute');
-
+const mainTemplateRoute = require("./mainTemplateRoute");
+const instancePolicy_01Route = require("./instancePolicy_01Route");
 
 
 
@@ -12,8 +13,9 @@ const motorRoute =require('./motorRoute');
 
 router.use('/company',lmCompanyRoutes);
 router.use('/businesses', class_of_busRoute);
-router.use('/businesses/motor/programs/PGIT6_01/instances/10-PD-01_01',motorRoute);
-
-
+// router.use('/businesses/motor/programs/PGIT6_01/instances/10-PD-01_01',motorRoute);
+router.use('/motor',motorRoute);
+router.use('/templates/policy/PGIT6_01',mainTemplateRoute);
+router.use('/instance/policy/PGIT6_01',instancePolicy_01Route);
 
 module.exports = router;
