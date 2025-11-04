3b48f1e0f5d111881812c33d4de23ca96c46e8f3
developer-wenxt <developer@wenxttech.com>
2025-11-04 14:41:42 +0530

    push

--- src/controllers/riskFieldController.js
+++ src/controllers/riskFieldController.js
@@ -1,29 +1,14 @@
 const { get } = require('../services/riskFieldService');
+const { successResponse, errorResponse } = require('../utils/response');
 
 exports.get = async (req, res) => {
   try {
-    const data = await get();
-    res.json(data);
+    const { section_code,product_code } = req.query;
+    const data = await get(section_code,product_code);
+
+    return successResponse(res, 200, 'Risk data fetched successfully', data);
   } catch (error) {
     console.error('Controller error:', error);
-    res.status(500).json({ error: 'Failed to fetch Risk Field' });
+    return errorResponse(res, 500, 'Failed to fetch Risk data', error.message || error);
   }
 };
-
-// const { get } = require('../services/riskFieldService');
-// const { successResponse, errorResponse } = require('../utils/response');
-
-// exports.get = async (req, res) => {
-//   try {
-//     const { progCode, blockName, modCode } = req.query;
-
-//     // Call the service with query parameters
-//     const data = await get(progCode, blockName, modCode);
-
-//     return successResponse(res, 200, 'Risk fields fetched successfully', data);
-//   } catch (error) {
-//     console.error('Error fetching risk fields:', error);
-//     return errorResponse(res, 500, 'Failed to fetch risk fields', error.message || error);
-//   }
-// };
-
