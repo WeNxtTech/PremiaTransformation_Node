ed52c6534e106f56ed52f9b76bc0780b30721574
developer-wenxt <developer@wenxttech.com>
2025-12-30 18:19:38 +0530

    search filter api and get list

--- src/services/PGITPOLRISKADDLINFOService.js
+++ src/services/PGITPOLRISKADDLINFOService.js
@@ -9,19 +9,15 @@ async function getNextPolSysId() {
   return result[0].NEXTVAL || result[0].nextVal;
 }
 
-
 exports.create = async (data) => {
-  // 1) Generate PK from sequence
   const nextId = await getNextPolSysId();
   data.prai_sys_id = nextId;
 
-  // 2) First insert (without PRAI_LVL1_SYS_ID or with null)
   const createdRecord = await PGITPOLRISKADDLINFO.create(data);
 
-  // 3) Now set PRAI_LVL1_SYS_ID = PRAI_SYS_ID in same row
-  //    (or any other logic you want)
+
   await createdRecord.update({
-    prai_lvl1_sys_id: createdRecord.prai_sys_id, // <== key line
+    prai_lvl1_sys_id: createdRecord.prai_sys_id,
   });
 
   const responseData = {
@@ -42,16 +38,6 @@ exports.create = async (data) => {
     data: responseData,
   };
 };
-// async function getNextPolSysId() {
-//   const [result] = await sequelize.query('SELECT PRAI_SYS_ID_SEQ.NEXTVAL AS nextVal FROM DUAL');
-//   return result[0].NEXTVAL || result[0].nextVal;  // depending on driver case
-// }
-// exports.create = async (data) => {
-//   const nextId = await getNextPolSysId();
-//   data.prai_sys_id = nextId;
-
-//   return await PGITPOLRISKADDLINFO.create(data);
-// };
 
 
 exports.update = async (id, updatedData) => {
