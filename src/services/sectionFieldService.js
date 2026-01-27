68b54b97dc1d71db05a88c1babf25e471ec93197
developer-wenxt <developer@wenxttech.com>
2026-01-27 15:33:59 +0530

    UPDATED SECTION FIELD API

--- src/services/sectionFieldService.js
+++ src/services/sectionFieldService.js
@@ -26,6 +26,7 @@ exports.get = async (prodCode) => {
       AND PARA_CODE = 'BUS_TYPE'
       AND PARA_SUB_CODE = POL_BUS_TYPE
       AND PROD_CODE = POL_PROD_CODE
+      AND PARA_NAME='Direct with coinsurance'
       AND  (POL_PROD_CODE = :prodCode)`;
    const records = await sequelize.query(query, {
     type: QueryTypes.SELECT,
