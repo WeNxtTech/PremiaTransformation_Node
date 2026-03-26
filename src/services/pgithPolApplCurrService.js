88cbfc4cb0f8e4042941d6f1d307f5f492880eb4
Agastin <developer@wenxttech.com>
2026-03-26 18:07:32 +0530

    other dtl history table getbyid done

---
+++ src/services/pgithPolApplCurrService.js
@@ -0,0 +1,46 @@
+const { PgithPolApplCurr } = require('../models');
+
+exports.getAll = async (filters, { limit = 10, offset = 0, order } = {}) => {
+  return PgithPolApplCurr.findAll({ where: filters, limit, offset, ...(order && { order }) });
+};
+
+exports.create = async (data) => {
+  return await PgithPolApplCurr.create(data);
+};
+
+exports.update = async (id, updatedData) => {
+  const item = await PgithPolApplCurr.findByPk(id);
+  if (!item) {
+    const error = new Error(`PgithPolApplCurr with ID ${id} not found`);
+    error.statusCode = 404;
+    throw error;
+  }
+  await item.update(updatedData);
+  return item;
+};
+
+exports.deleteItem = async (id) => {
+  const item = await PgithPolApplCurr.findByPk(id);
+  if (!item) {
+    const error = new Error(`PgithPolApplCurr with ID ${id} not found`);
+    error.statusCode = 404;
+    throw error;
+  }
+  await item.destroy();
+  return item;
+};
+
+exports.getByPolSysId = async (PACH_POL_SYS_ID) => {
+  const items = await PgithPolApplCurr.findAll({
+    where: { PACH_POL_SYS_ID },
+    raw: true
+  });
+
+  const groupedResult = items.reduce((acc, row) => {
+    const key = row.PACH_END_NO_IDX; 
+    (acc[key] ??= []).push(row);
+    return acc;
+  }, {});
+
+  return groupedResult;
+};
