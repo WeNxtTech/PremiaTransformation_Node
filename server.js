7a6cff305f0d71567ea8eb5dcf5567317d6e73c4
Dinesh <dinesh.b@wenxttech.com>
2026-04-03 15:58:58 +0530

    pushing to main

--- server.js
+++ server.js
@@ -2,15 +2,27 @@ require('dotenv').config();
 const fs = require('fs');
 const https = require('https');
 const app = require('./src/app');
-
+ 
 const PORT = process.env.PORT || 443;
 const HOST = process.env.HOST || '0.0.0.0';
-
+ 
 const sslOptions = {
   key: fs.readFileSync("C:/nginx/conf/certs/wecorelite/wecorelite.wenxttech.com-key.pem"),
   cert: fs.readFileSync("C:/nginx/conf/certs/wecorelite/wecorelite.wenxttech.com-chain.pem"),
 };
-
-https.createServer(sslOptions, app).listen(PORT, HOST, () => {
+ 
+// https.createServer(sslOptions, app).listen(PORT, HOST, () => {
+//   console.log(`HTTPS Server running at https://${HOST}:${PORT}`);
+// });
+ 
+ 
+ 
+const server = https.createServer(sslOptions, app);
+ 
+ 
+server.timeout = 300000;
+server.listen(PORT, HOST, () => {
   console.log(`HTTPS Server running at https://${HOST}:${PORT}`);
 });
+ 
+ 
